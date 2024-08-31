bfd1fceb740ad9ac908aa2cce91d509eb2d5c263
DESKTOP-AU5311P\Gusenitsa <n.itchy.net@gmail.com>
2024-08-31 18:49:02 +0300

    Агрегация по регионам

--- docker/migrations/002_aggregation_by_region_mv.sql
+++ docker/migrations/002_aggregation_by_region_mv.sql
@@ -108,7 +108,7 @@ create table sanitation_of_settlements.collection_and_disposal_per_region (
 order by (year, region_id, district_id)
 comment 'Раздел 6. Сбор, вывоз и размещение коммунальных отходов. По районам';
 
-create materialized view sanitation_of_settlements.sorting_and_recycling_detailed_per_region_mv to sanitation_of_settlements.collection_and_disposal_per_region
+create materialized view sanitation_of_settlements.collection_and_disposal_per_region_mv to sanitation_of_settlements.collection_and_disposal_per_region
 as select
     source.year as year,
     source.district_id as district_id,
@@ -119,7 +119,7 @@ group by year, region_id, district_id;
 
 --###
 
-create table sanitation_of_settlements.preparation_and_transportation (
+create table sanitation_of_settlements.preparation_and_transportation_per_region (
     values AggregateFunction(sumForEach, Array(Int64)),
     year Int64,
     district_id Int64,
@@ -139,14 +139,14 @@ group by year, region_id, district_id;
 
 --###
 
-create table sanitation_of_settlements.main_performance_indicators (
+create table sanitation_of_settlements.main_performance_indicators_per_region (
     values AggregateFunction(sumForEach, Array(Int64)),
     year Int64,
     district_id Int64,
     region_id Int64
 ) engine AggregatingMergeTree()
 order by (year, region_id, district_id)
-comment 'Раздел 7. Сбор, заготовка и сдача вторичного сырья. По районам';
+comment 'Раздел 8. Основные показатели финансово-хозяйственной деятельности. По районам';
 
 create materialized view sanitation_of_settlements.main_performance_indicators_per_region_mv to sanitation_of_settlements.main_performance_indicators_per_region
 as select
@@ -155,4 +155,24 @@ as select
     source.region_id as region_id,
     sumForEachState(source.values * source.sign) as values
 from sanitation_of_settlements.main_performance_indicators as source
+group by year, region_id, district_id;
+
+--###
+
+create table sanitation_of_settlements.additional_per_region (
+    values AggregateFunction(sumForEach, Array(Int64)),
+    year Int64,
+    district_id Int64,
+    region_id Int64
+) engine AggregatingMergeTree()
+order by (year, region_id, district_id)
+comment 'Справочно. По районам';
+
+create materialized view sanitation_of_settlements.additional_per_region_mv to sanitation_of_settlements.additional_per_region
+as select
+    source.year as year,
+    source.district_id as district_id,
+    source.region_id as region_id,
+    sumForEachState(source.values * source.sign) as values
+from sanitation_of_settlements.additional as source
 group by year, region_id, district_id;
